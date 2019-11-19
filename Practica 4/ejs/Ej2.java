5368ec93e47988f0aa2ac47dcaa0919855ebf4a8
pedroodb <dalbianco.pedro@gmail.com>
2019-11-19 18:46:17 -0300

    Funciona ej2

--- Practica 4/ejs/Ej2.java
+++ Practica 4/ejs/Ej2.java
@@ -1,28 +1,49 @@
 import jade.core.*;
-public class Ej2 extends Agent
-{
-// Ejecutado por unica vez en la creacion
-public void setup()
-{
-	Location origen = here();
-	System.out.println("\n\nHola, agente con nombre local " + getLocalName());
-	System.out.println("Y nombre completo... " + getName());
-	System.out.println("Y en location " + origen.getID() + "\n\n");
-// Para migrar el agente
-try {
-	ContainerID destino = new ContainerID("Main-Container", null);
-	System.out.println("Migrando el agente a " + destino.getID());
-	doMove(destino);
-} catch (Exception e) {
-	System.out.println("\n\n\nNo fue posible migrar el agente\n\n\n");}
-}
 
-// Ejecutado al llegar a un contenedor como resultado de una migracion
-protected void afterMove()
-{
-	Location origen = here();
-	System.out.println("\n\nHola, agente migrado con nombre local " + getLocalName());
-	System.out.println("Y nombre completo... " + getName());
-	System.out.println("Y en location " + origen.getID() + "\n\n");
-}
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Ej2 extends Agent {
+
+	Location origin;
+	String destiny;
+	String file;
+	int result = 0;
+
+	// Ejecutado por unica vez en la creacion
+	public void setup() {
+		this.origin = here();
+		this.destiny = (String)getArguments()[0];
+		this.file = (String)getArguments()[1];
+		
+		System.out.println("\n\nHola, agente con nombre local " + getLocalName());
+		System.out.println("Se conectara a " + this.destiny + " para leer el archivo " + this.file);
+		// Para migrar el agente
+		try {
+			ContainerID destino = new ContainerID(this.destiny, null);
+			System.out.println("Migrando el agente a " + destino.getID() + "\n\n\n");
+			doMove(destino);
+		} catch (Exception e) {
+			System.out.println("\n\n\nNo fue posible migrar el agente\n\n\n");
+		}
+	}
+
+	// Ejecutado al llegar a un contenedor como resultado de una migracion
+	protected void afterMove() {
+		if(here().getID().equals(this.origin.getID())) {
+			System.out.println("\n\n\nEl resultado es " + this.result + "\n\n\n");
+		} else {
+			System.out.println("\n\nHola, agente migrado con nombre local " + getLocalName());
+			System.out.println("se tratara de acceder al archivo " + this.file);
+			try {
+				String[] numbers = new String(Files.readAllBytes(Paths.get(this.file))).split(",");
+				for(String n : numbers){
+					this.result += Integer.parseInt(n);
+				}
+			} catch (Exception e) {
+				this.result = -1;
+			}
+			doMove(this.origin);
+		}
+	}
 }
